0d9dd87f30879e95f0d6898d3e162c280369c4f6
johan <johan.alamo@gmail.com>
2019-10-22 18:19:53 -0300

    add documentation for Coord.kt

--- src/main/kotlin/com/alamo/utilities/Coord.kt
+++ src/main/kotlin/com/alamo/utilities/Coord.kt
@@ -1,3 +1,9 @@
 package com.alamo.utilities
 
+/**
+ * The Coord class  defines a simple coordinate. The main purpose is to be used in a Matrix
+ *
+ * @property row The row value
+ * @property col The col value
+ */
 data class Coord (val row:Int,   val col:Int)
